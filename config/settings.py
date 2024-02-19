a1ccdc46a2cf74d8395f2d658020fb8045e9e51d
Maxim Abavi <afghan1363@ya.ru>
2024-02-19 21:08:45 +0500

    Dockerfile and docker-compose.yaml

--- config/settings.py
+++ config/settings.py
@@ -177,8 +177,9 @@ SWAGGER_SETTINGS = {
 }
 STRIPE_API_KEY = os.getenv('stripe_api_key')
 
-CELERY_BROKER_URL = 'redis://redis:6379/0'
-CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
+CELERY_BROKER_URL = os.getenv('REDIS_LOCATION')
+CELERY_RESULT_BACKEND = os.getenv('REDIS_LOCATION')
+
 CELERY_BEAT_SCHEDULE = {
     'check_user_activity': {
         'task': 'lms_app.tasks.inactive_user',
